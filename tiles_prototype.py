8b11dbbc12564f06be91fdc6784cbfc9d89146f8
azarz <amaury.zarzelli@gmail.com>
2018-05-04 16:28:52 +0200

    no more alignment pbs, thread limit pbs

--- tiles_prototype.py
+++ tiles_prototype.py
@@ -1,14 +1,15 @@
-import multiprocessing as mp
-import multiprocessing.pool
-import threading
-import os
-import queue
 from pathlib import Path
 import functools
+import multiprocessing as mp
+import multiprocessing.pool
+import os
 import pickle
+import queue
+import threading
 
-import numpy as np
+from keras.models import load_model
 import buzzard as buzz
+import numpy as np
 
 from show_many_images import show_many_images
 from uids_of_paths import uids_of_paths
@@ -17,7 +18,9 @@ from watcher import Watcher
 
 class MultiThreadedRasterResampler(object):
 
-    def __init__(self, path, scale, cache_dir="./.cache"):
+    def __init__(self, path, scale, rtype, cache_dir="./.cache", get_slopes=False):
+
+        self.get_slopes = get_slopes
 
         self.lock = threading.Lock()
         self.cv = threading.Condition()
@@ -38,13 +41,13 @@ class MultiThreadedRasterResampler(object):
         ds = buzz.DataSource(allow_interpolation=True)
 
         with ds.open_araster(self.raster_path).close as raster:
-            self.file_fp = raster.fp.intersection(raster.fp, scale=scale, alignment=(0,0))
-            tile_count = np.ceil(self.file_fp.rsize / 500) 
-            self.cache_tiles_fps = self.file_fp.tile_count(*tile_count, boundary_effect='shrink')
+            self.full_fp = raster.fp.intersection(raster.fp, scale=scale, alignment=(0,0))
+            tile_count = np.ceil(self.full_fp.rsize / 500) 
+            self.cache_tiles_fps = self.full_fp.tile_count(*tile_count, boundary_effect='shrink')
             self.num_bands = len(raster)
 
         self.cache_tile_paths = [
-            str(Path(cache_dir) / dir_names[frozenset({'ortho'})] / str(str(fp.tlx) + "_" + str(fp.tly) + ".tif"))
+            str(Path(cache_dir) / dir_names[frozenset({rtype})] / str(str(fp.tlx) + "_" + str(fp.tly) + ".tif"))
             for fp in self.cache_tiles_fps.flat
         ]
 
@@ -55,7 +58,7 @@ class MultiThreadedRasterResampler(object):
         self.req_q.put(None)
 
 
-    def _callback_dico(self, args,res):
+    def _callback_dico(self, args, res):
         with self.lock:
             self.dico[args[1]] = res
         with self.cv:
@@ -69,41 +72,69 @@ class MultiThreadedRasterResampler(object):
             if args == None:
                 return
 
-            try:
-                with self.lock:
+            with self.lock:
+                started = args[1] in self.dico.keys()
+                file_exists = os.path.isfile(args[1])
+
+                # Starded
+                if started:
                     value = self.dico[args[1]]
-                # Computing
-                if isinstance(value, int):
-                    # In both fileExists and !fileExists cases, we wait
-                    self.computation_pool.apply_async(self._wait_for_resampling, (args[1],))
-                    
-                # Computed
+
+                    # Computing
+                    if isinstance(value, int):
+                        # In both fileExists and !fileExists cases, we wait
+                        self.computation_pool.apply_async(self._wait_for_resampling, (args[1],))
+                        
+                    # Computed
+                    else:
+                        # fileExists
+                        if os.path.isfile(args[1]):
+                            # ReadingFile
+                            self.io_pool.apply_async(self._get_tile_data, (args[1],), callback=functools.partial(self._callback_dico, args))
+                        # !fileExists
+                        else:
+                            # Should not happen
+                            raise RuntimeError("There should be a file")
+
+                # NotStarted
                 else:
                     # fileExists
-                    if os.path.isfile(args[1]):
-                        # ReadingFile
+                    if file_exists:
+                        # Reading file
+                        self.dico[args[1]] = 0
                         self.io_pool.apply_async(self._get_tile_data, (args[1],), callback=functools.partial(self._callback_dico, args))
                     # !fileExists
                     else:
-                        # Should not happen
-                        raise RuntimeError("There should be a file")
-
-            # NotStarted
-            except KeyError:
-                # fileExists
-                if os.path.isfile(args[1]):
-                    # Reading file
-                    with self.lock:
+                        # Writing file
                         self.dico[args[1]] = 0
-                    self.io_pool.apply_async(self._get_tile_data, (args[1],), callback=functools.partial(self._callback_dico, args))
-                # !fileExists
-                else:
-                    # Writing file
-                    with self.lock:
-                        self.dico[args[1]] = 0
-                    self.computation_pool.apply_async(self._resample_tile, args, callback=functools.partial(self._callback_dico, args))
+                        self.computation_pool.apply_async(self._resample_tile, args, callback=functools.partial(self._callback_dico, args))
 
 
+
+    def _get_slopes(self, proxy):
+        fp = proxy.fp
+        arr = proxy.get_data(fp=fp.dilate(1))
+        nodata_mask = arr == proxy.nodata
+        nodata_mask = ndi.binary_dilation(nodata_mask)
+        kernel = [
+            [0, 1, 0],
+            [1, 1, 1],
+            [0, 1, 0],
+        ]
+        arru = ndi.maximum_filter(arr, None, kernel) - arr
+        arru = np.arctan(arru / fp.pxsizex)
+        arru = arru / np.pi * 180.
+        arru[nodata_mask] = 0
+        arru = arru[1:-1, 1:-1]
+
+        arrd = arr - ndi.minimum_filter(arr, None, kernel)
+        arrd = np.arctan(arrd / fp.pxsizex)
+        arrd = arrd / np.pi * 180.
+        arrd[nodata_mask] = 0
+        arrd = arrd[1:-1, 1:-1]
+
+        arr = np.dstack([arrd, arru])
+        return arr
 
     def _wait_for_resampling(self, dummy_value):
         with self.cv:
@@ -120,23 +151,26 @@ class MultiThreadedRasterResampler(object):
             ds = self.thread_storage.ds
 
         with ds.open_araster(tile_path).close as raster:
-            out = raster.get_data(band=-1)
+            if self.get_slopes:
+                out = self._get_slopes(raster)
+            else:
+                out = raster.get_data(band=-1)
         return out
 
 
-    def _merge_out_tiles(self, tiles, results, out_fp):
-
+    def _merge_out_tiles(self, tiles, data, out_fp):
         if self.num_bands > 1:
             out = np.empty(tuple(out_fp.shape) + (self.num_bands,), dtype="uint8")
         else:
             out = np.empty(tuple(out_fp.shape), dtype="float32")
 
-        for new, out_tile in zip(results, tiles):
-            out[out_tile.slice_in(out_fp, clip=True)] = new[out_fp.slice_in(out_tile, clip=True)]
+        for tile, dat  in zip(tiles, data):
+            out[tile.slice_in(out_fp, clip=True)] = dat[out_fp.slice_in(tile, clip=True)]
         return out
 
 
     def _resample_tile(self, tile_fp, tile_path):
+        print("resample in")
         if not hasattr(self.thread_storage, "ds"):
             ds = buzz.DataSource(allow_interpolation=True)
             self.thread_storage.ds = ds
@@ -148,9 +182,14 @@ class MultiThreadedRasterResampler(object):
             out = src.get_data(band=-1, fp=tile_fp)
             if len(src) == 4:
                 out = np.where((out[...,3] == 255)[...,np.newaxis], out, 0)
+
+        print("resample")
         out_proxy = ds.create_araster(tile_path, tile_fp, src.dtype, len(src), driver="GTiff", band_schema={"nodata": src.nodata}, sr=src.wkt_origin)
         out_proxy.set_data(out, band=-1)
+        if self.get_slopes:
+            out = self._get_slopes(out_proxy)
         out_proxy.close()
+        print("resample out")
         return out
 
 
@@ -184,11 +223,34 @@ class MultiThreadedRasterResampler(object):
         return self._merge_out_tiles(intersecting_tiles, input_data, input_fp)
 
 
+
+
+
+
+
+
+
+
+def output_fp_to_input_fp(fp, scale, rsize):
+    out = buzz.Footprint(tl=fp.tl, size=fp.size, rsize=fp.size/scale)
+    padding = (rsize - out.rsizex) / 2
+    assert padding == int(padding)
+    out = out.dilate(padding)
+    return out
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     print("hello")
 
     rgb_path = "./ortho_8.00cm.tif"
     dsm_path = "./dsm_8.00cm.tif"
+    model_path = "./18-01-25-15-38-19_1078_1.00000000_0.07799472_aracena.hdf5"
 
     print("uids...")
     dir_names = uids_of_paths({
@@ -196,17 +258,53 @@ if __name__ == "__main__":
             "dsm": dsm_path
         })
 
-    with MultiThreadedRasterResampler(rgb_path, 0.64) as test_obj:
+    cache_dir = "./.cache"
 
-        input_tiles = test_obj.file_fp.tile(np.asarray((1030, 1030)))
+    for path in dir_names.values():
+        os.makedirs(str(Path(cache_dir) / path), exist_ok=True)
 
-        test = test_obj.computation_pool.apply_async(test_obj.get_data, (input_tiles[0, 0],))
-        test2 = test_obj.computation_pool.apply_async(test_obj.get_data, (input_tiles[0, 0],))
+    datasrc = buzz.DataSource(allow_interpolation=True)
 
-        out_array = test.get()
-        test2.get()
+    print("model...")
+    model = load_model(model_path)
 
-        show_many_images(
-            [out_array], 
-            extents=[input_tiles[0, 0].extent]
-        )
+
+    with datasrc.open_araster(rgb_path).close as raster:
+        out_fp = raster.fp.intersection(raster.fp, scale=1.28, alignment=(0,0))
+
+    out_fp = out_fp.intersection(out_fp, scale=0.64)
+
+    out_tiles = out_fp.tile(np.asarray(model.outputs[0].shape[1:3]).T)
+    print(out_tiles[0,0])
+
+    rgba_tiles = np.asarray([
+        output_fp_to_input_fp(tile, 0.64, model.get_layer("rgb").input_shape[1]) 
+        for tile in out_tiles.flatten()
+    ]).reshape(out_tiles.shape)
+
+    dsm_tiles = np.asarray([
+        output_fp_to_input_fp(tile, 1.28, model.get_layer("slopes").input_shape[1]) 
+        for tile in out_tiles.flatten()
+    ]).reshape(out_tiles.shape)
+
+    print("overhead done!")
+
+    with MultiThreadedRasterResampler(rgb_path, 0.64, 'ortho', cache_dir) as rgb_resampler:
+        with MultiThreadedRasterResampler(dsm_path, 1.28, 'dsm', cache_dir, get_slopes=False) as dsm_resampler:
+
+            rgb_results = []
+            slopes_results = []
+
+            for tile_index in range(1):
+                # rgb_results.append(rgb_resampler.computation_pool.apply_async(rgb_resampler.get_data, (rgba_tiles.flat[tile_index],)))
+                slopes_results.append(rgb_resampler.computation_pool.apply_async(dsm_resampler.get_data, (dsm_tiles.flat[tile_index],)))
+
+            for result_index in range(1):
+                # rgb_results[result_index].get()
+                slopes_results[result_index].get()
+
+
+            # show_many_images(
+            #     [out_arrays[0]], 
+            #     extents=[input_tiles[0, 0].extent]
+            # )
