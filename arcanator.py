22ad3106b6df427bd0b262c5d3c552ace76e3ce6
azarz <amaury.zarzelli@gmail.com>
2018-05-23 17:16:05 +0200

    HMRaster OK

--- arcanator.py
+++ arcanator.py
@@ -257,7 +257,7 @@ class AbstractCachedRaster(AbstractRaster):
 
     def _write_cache_data(self, cache_tile, data):
         filepath = self._get_cache_tile_path(cache_tile)
-        out_proxy = ds.create_araster(filepath, cache_tile, data.dtype, self._num_bands, driver="GTiff", sr=self._resampled_rgba.wkt_origin)
+        out_proxy = ds.create_araster(filepath, cache_tile, data.dtype, self._num_bands, driver="GTiff", sr=self._primitives[0].wkt_origin)
         out_proxy.set_data(data, band=-1)
         out_proxy.close()
 
@@ -476,19 +476,20 @@ class Slopes(AbstractNotCachedRaster):
 
 class HeatmapRaster(AbstractCachedRaster):
 
-    def __init__(self, model, resampled_rgba, slopes, DIR_NAMES, cache_dir="./.cache"):
-
-        self._model = model
-        self._num_bands = LABEL_COUNT
-
-        self._resampled_rgba = resampled_rgba
-        self._slopes = slopes
+    def __init__(self, model, resampled_rgba, slopes, cache_dir="./.cache"):
 
         max_scale = max(resampled_rgba.fp.scale[0], slopes.fp.scale[0])
         min_scale = min(resampled_rgba.fp.scale[0], slopes.fp.scale[0])
       
         self._full_fp = resampled_rgba.fp.intersection(slopes.fp, scale=max_scale, alignment=(0, 0))
 
+        super.__init__(self._full_fp, ("dsm", "ortho"))
+
+        self._model = model
+        self._num_bands = LABEL_COUNT
+
+        self._primitives = [resampled_rgba, slopes]
+      
         self._full_fp = self._full_fp.intersection(self._full_fp, scale=min_scale)
 
         self._computation_tiles = self._full_fp.tile(np.asarray(model.outputs[0].shape[1:3]).T)
@@ -496,20 +497,7 @@ class HeatmapRaster(AbstractCachedRaster):
         tile_count = np.ceil(self._full_fp.rsize / 500) 
         self._cache_tiles_fps = self._full_fp.tile_count(*tile_count, boundary_effect='shrink')
 
-        self._double_tiled_structure = DoubleTiledStructure(list(self._cache_tiles_fps.flat), list(self._computation_tiles.flat), self._computation_method)
-
-        self._cache_tile_paths = [
-            str(Path(cache_dir) / DIR_NAMES[frozenset({"dsm", "ortho"})] / str(str(np.around(fp.tlx, 2)) + "_" + 
-                                                                        str(np.around(fp.tly, 2)) + "_" + 
-                                                                        str(np.around(fp.pxsizex, 2)) + "_" + 
-                                                                        str(np.around(fp.rsizex, 2)))
-            )
-            for fp in self._cache_tiles_fps.flat
-        ]
-
-
-        self._lock = threading.Lock()
-        self._thread_storage = threading.local()
+        self._double_tiled_structure = DoubleTiledStructure(list(self._cache_tiles_fps.flat), list(self._computation_tiles.flat), self._computation_method, 1)
 
 
     def _computation_method(self, computation_tile):
@@ -517,8 +505,8 @@ class HeatmapRaster(AbstractCachedRaster):
         rgba_tile = output_fp_to_input_fp(computation_tile, 0.64, self._model.get_layer("rgb").input_shape[1])
         dsm_tile = output_fp_to_input_fp(computation_tile, 1.28, self._model.get_layer("slopes").input_shape[1])
 
-        rgba_data = self._resampled_rgba.get_data(rgba_tile)
-        slope_data = self._slopes.get_data(dsm_tile)
+        rgba_data = self._primitives[0].get_data(rgba_tile)
+        slope_data = self._primitives[1].get_data(dsm_tile)
 
         rgb_data = np.where((rgba_data[...,3] == 255)[...,np.newaxis], rgba_data, 0)[...,0:3]
         rgb = (rgb_data.astype('float32') - 127.5) / 127.5
@@ -528,70 +516,6 @@ class HeatmapRaster(AbstractCachedRaster):
         prediction = self._model.predict([rgb[np.newaxis], slopes[np.newaxis]])[0]
 
         return prediction
-
-
-
-    def _merge_out_tiles(self, tiles, data, out_fp):
-
-        out = np.empty(tuple(out_fp.shape) + (self._num_bands,), dtype="float32")
-
-        for tile, dat  in zip(tiles, data):
-            assert tile.same_grid(out_fp)
-            out[tile.slice_in(out_fp, clip=True)] = dat[out_fp.slice_in(tile, clip=True)]
-        return out
-
-
-
-    def get_data(self, input_fp):
-
-        output_data = []
-        intersecting_tiles = []
-
-        if not hasattr(self._thread_storage, "ds"):
-            ds = buzz.DataSource(allow_interpolation=True)
-            self._thread_storage.ds = ds
-
-        else:
-            ds = self._thread_storage.ds
-
-        def tile_info_gen():
-            for cache_tile, filename in zip(self._cache_tiles_fps.flat, self._cache_tile_paths):
-                if cache_tile.share_area(input_fp):
-                    yield cache_tile, filename
-
-        for cache_tile, filepath in tile_info_gen():
-
-            file_exists = os.path.isfile(filepath)
-            
-            if not file_exists:
-
-                with self._lock:
-                    # checking again if the file exists (happens when entering the lock after waiting)
-                    file_exists = os.path.isfile(filepath)
-
-                    if not file_exists:
-                        print("--> using GPU...")
-                        prediction = self._double_tiled_structure.get_cache_data(cache_tile)
-                        print("<-- no more GPU")
-
-                        out_proxy = ds.create_araster(filepath, cache_tile, "float32", LABEL_COUNT, driver="GTiff", sr=self._resampled_rgba.wkt_origin)
-                        out_proxy.set_data(prediction, band=-1)
-                        out_proxy.close()
-
-                if file_exists:
-                    print("!! cache was calculated when i was waiting")
-                    with ds.open_araster(filepath).close as src:
-                        prediction = src.get_data(band=-1)
-
-            else:
-                with ds.open_araster(filepath).close as src:
-                    prediction = src.get_data(band=-1)
-
-            intersecting_tiles.append(cache_tile)
-            output_data.append(prediction)
-
-        return self._merge_out_tiles(intersecting_tiles, output_data, input_fp)
-
 
 
 
@@ -627,8 +551,8 @@ def main():
     initial_rgba = datasrc.open_araster(rgb_path)
     initial_dsm = datasrc.open_araster(dsm_path)
 
-    resampled_rgba = ResampledOrthoimage(rgb_path, 0.64, DIR_NAMES, cache_dir)
-    resampled_dsm = ResampledDSM(dsm_path, 1.28, DIR_NAMES, cache_dir)
+    resampled_rgba = ResampledOrthoimage(initial_rgba, 0.64, DIR_NAMES, cache_dir)
+    resampled_dsm = ResampledDSM(initial_dsm, 1.28, DIR_NAMES, cache_dir)
 
     slopes = Slopes(resampled_dsm)
 
