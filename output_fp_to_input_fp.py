f430481a52b755c84be2c68ec002882426e44822
azarz <amaury.zarzelli@gmail.com>
2018-06-15 10:00:15 +0200

    better with the actual file...

---
+++ output_fp_to_input_fp.py
@@ -0,0 +1,19 @@
+"""
+Creates a footprint of rsize from fp using dilation and scale
+Used to compute a keras model input footprint from output
+(e.g.: deduce the big RGB and slopes extents from the smaller output heatmap)
+"""
+
+import buzzard as buzz
+
+def output_fp_to_input_fp(fp, scale, rsize):
+    """
+    Creates a footprint of rsize from fp using dilation and scale
+    Used to compute a keras model input footprint from output
+    (e.g.: deduce the big RGB and slopes extents from the smaller output heatmap)
+    """
+    out = buzz.Footprint(tl=fp.tl, size=fp.size, rsize=fp.size/scale)
+    padding = (rsize - out.rsizex) / 2
+    assert padding == int(padding)
+    out = out.dilate(padding)
+    return out
