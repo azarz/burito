f201a281c3a6d4599ab1cd70b92e68aab94fa588
azarz <amaury.zarzelli@gmail.com>
2018-06-21 12:18:57 +0200

    rounding fix

--- output_fp_to_input_fp.py
+++ output_fp_to_input_fp.py
@@ -3,7 +3,7 @@ Creates a footprint of rsize from fp using dilation and scale
 Used to compute a keras model input footprint from output
 (e.g.: deduce the big RGB and slopes extents from the smaller output heatmap)
 """
-
+import numpy as np
 import buzzard as buzz
 
 def output_fp_to_input_fp(fp, scale, rsize):
@@ -12,7 +12,7 @@ def output_fp_to_input_fp(fp, scale, rsize):
     Used to compute a keras model input footprint from output
     (e.g.: deduce the big RGB and slopes extents from the smaller output heatmap)
     """
-    out = buzz.Footprint(tl=fp.tl, size=fp.size, rsize=fp.size/scale)
+    out = buzz.Footprint(tl=fp.tl, size=fp.size, rsize=np.around(fp.size/scale))
     padding = (rsize - out.rsizex) / 2
     assert padding == int(padding)
     out = out.dilate(padding)
