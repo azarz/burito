dc2ddaacd846a91996ef67d3a27cc96bb73fb5c1
Ngoguey42 <ngoguey@airware.com>
2018-07-02 11:50:12 +0200

    More work on resampled rasters

--- custom_rasters.py
+++ custom_rasters.py
@@ -20,8 +20,62 @@ def _Raster(**kwargs):
         kwargs['nbands'] = kwargs['band_count']
         del kwargs['band_count']
 
-    pprint(kwargs)
+    # pprint(kwargs)
     return Raster(**kwargs)
+
+
+def derive_raster(raster, **kwargs):
+    if 'fp' not in kwargs:
+        kwargs['fp'] = raster.fp
+
+    if 'dtype' not in kwargs:
+        kwargs['dtype'] = raster.dtype
+    kwargs['band_count'] = len(raster)
+    kwargs['nodata'] = raster.nodata
+    if 'sr' not in kwargs:
+        kwargs['sr'] = raster.wkt_origin
+
+    if 'computation_function' not in kwargs:
+        # resample = not kwargs['fp'].same_grid(raster.fp)
+        kwargs['primitives'] = {'src': raster.get_multi_data_queue}
+        primitive_fp = raster.fp
+        primitive_nodata = raster.nodata
+
+        def make_primitive_fps(fp):
+            return {
+                'src': (primitive_fp & fp).dilate(4),
+            }
+
+        def compute_data(fp, parrs, pfps, _):
+            # assert fp.poly.within(pfps[0].poly)
+            arr = buzz.Raster._remap(
+                pfps[0], fp, parrs[0], nodata=primitive_nodata,
+            )
+            return arr
+
+        kwargs['computation_function'] = compute_data
+        kwargs['to_collect_of_to_compute'] = make_primitive_fps
+
+    if 'cache_dir' in kwargs and kwargs['cache_dir'] is not None:
+        kwargs['cached'] = True
+        # kwargs['overwrite'] =
+        # kwargs['cache_dir'] =
+        if 'cache_fps' not in kwargs or 'cache_fps' is None:
+            kwargs['cache_fps'] = kwargs['fp'].tile(
+                (512, 512),
+                boundary_effect='shrink',
+            )
+    else:
+        kwargs['cached'] = False
+        # kwargs['overwrite'] =
+
+    kwargs['io_pool'] = g_io_pool
+    kwargs['computation_pool'] = g_cpu_pool
+    kwargs['computation_fps'] = None
+    kwargs['merge_pool'] = None
+    kwargs['merge_function'] = None
+
+    return _Raster(**kwargs)
 
 
 def wrap_buzzard_raster(buzz_raster, **kwargs):
@@ -37,7 +91,6 @@ def wrap_buzzard_raster(buzz_raster, **kwargs):
     if 'sr' not in kwargs:
         kwargs['sr'] = buzz_raster.wkt_origin
 
-    # thread_storage = threading.local()
     path = buzz_raster.path
     def compute_data(compute_fp, *args):
         if resample:
@@ -51,7 +104,6 @@ def wrap_buzzard_raster(buzz_raster, **kwargs):
         return arr
 
     kwargs['computation_function'] = compute_data
-
 
     if 'cache_dir' in kwargs and kwargs['cache_dir'] is not None:
         kwargs['cached'] = True
--- main2.py
+++ main2.py
@@ -1,4 +1,5 @@
 import os
+import collections
 
 from pytilia.visorbearer import *
 from pytilia.watcher import *
@@ -26,27 +27,49 @@ def main():
     ortho_path = os.path.join(pref, q, ortho_suff)
 
     # ******************************************************************************************* **
+    # Open base data
     ds = buzz.DataSource()
     with ds.open_araster(dsm_path).close as r:
         dsm = wrap_buzzard_raster(r)
     with ds.open_araster(ortho_path).close as r:
         ortho = wrap_buzzard_raster(r)
 
+    # ******************************************************************************************* **
+    # Open resampled rasters
+    dsms = collections.OrderedDict()
+    r = dsm
+    scales = [0.16, 0.32, 0.64, 1.28]
+    # for scale in [0.16, 0.32]:
+    for scale in scales:
+        rr = derive_raster(r, fp=r.fp.intersection(r.fp, scale=scale, alignment=(0, 0)).erode(4))
+        dsms[scale] = rr
+        r = rr
 
     imgs = [
         dsm.get_multi_data([dsm.fp]),
-        ortho.get_multi_data([ortho.fp]),
+        dsms[0.16].get_multi_data([dsms[0.16].fp]),
+        dsms[0.32].get_multi_data([dsms[0.32].fp]),
+        dsms[0.64].get_multi_data([dsms[0.64].fp]),
+        dsms[1.28].get_multi_data([dsms[1.28].fp]),
+        # dsm32.get_multi_data([dsm32.fp]),
+        # ortho.get_multi_data([ortho.fp]),
     ]
-    extents = [
-        dsm.fp.extent,
-        ortho.fp.extent,
-    ]
+    # + [r.get_multi_data([r.fp]) for r in dsms.values()]
+    extents = [dsm.fp.extent]  * len(imgs)
+
+        # dsm16.fp.extent,
+        # dsm32.fp.extent,
+        # ortho.fp.extent,
+    # ]
+    # + [r.fp.extent for r in dsms.values()]
 
     imgs = list(map(next, imgs))
 
 
-    nodata_mask = imgs[0] == dsm.nodata
-    imgs[0][nodata_mask] = imgs[0][~nodata_mask].min()
+    for img in imgs:
+        if img.ndim == 2:
+            nodata_mask = img == dsm.nodata
+            img[nodata_mask] = img[~nodata_mask].min()
     # show_many_images(imgs, patchess=(), extents=(), batches=1, subtitles=(), cmaps=(), title='', ratio=1.7777777777777777, mode='display', offset=True, visible_axes=True, share_scale=False)
 
     show_many_images(imgs, extents=extents)
