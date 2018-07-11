b0ce9e17ee9e37b284e80b43e7182597202f48cc
azarz <amaury.zarzelli@gmail.com>
2018-07-11 11:44:02 +0200

    new location of checksum: the attic

---
+++ old_py_scripts/checksum.py
@@ -0,0 +1,38 @@
+"""
+Computes the checksum of an array
+"""
+import numpy as np
+import buzzard as buzz
+
+def checksum(array):
+    """
+    Computes the checksum of an array
+    """
+
+    # Reinterpret array as unsigned integers
+    b = array.view({
+        4: np.uint32,
+        8: np.uint64,
+        1: np.uint8,
+        2: np.uint16
+    }[array.dtype.itemsize])
+
+    # Convert to 64 bit
+    b = b.astype('uint64')
+
+    # Sum
+    cs = int(b.sum())
+
+    # Merge the first and second half
+    cs = ((cs >> 32) ^ (cs >> 0)) & (2 ** 32 - 1)
+    return cs
+
+
+def checksum_file(path):
+    """
+    Computes the checksum of a file
+    """
+    ds = buzz.DataSource()
+    with ds.open_araster(path).close as raster:
+        cs = checksum(raster.get_data(band=-1))
+    return cs
